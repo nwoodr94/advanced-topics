b567434465efd41f4007fca362c749d118be6995
Nathan Woodruff <nwoodr94@gmail.com>
2020-05-21 23:31:06 -0700

    full-stack up

---
+++ webapi/controller/helpers.js
@@ -0,0 +1,19 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('../db/db.json')
+const db = low(adapter)
+
+
+function getUser(id) {
+
+    user = db.get('users')
+      .find({ _id: id })
+      .value();
+      
+    return user;
+};
+
+module.exports = {
+    getUser: getUser
+};
---
+++ webapi/server.js
@@ -0,0 +1,19 @@
+// Express
+const express = require("express");
+const app = express();
+const controller = require('./controller/user-controller.js');
+
+// Configuration
+const port = 8000;
+const host = 'localhost';
+
+// Middleware
+const cors = require('cors');
+const corsOptions = { origin: '*' };
+app.use(cors(corsOptions));
+
+// Router
+app.use('/', controller);
+
+// Server
+app.listen(port, host, () => console.log(`Server listening at ${host}:${port}`));
